4e4c309e0cca6bba1d8a1750e27f1dd21828fa5e
Maven Lu <milu@ebay.com>
2014-04-11 13:26:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'a5881bf51d3636463b10c8ec434c59e87a297ce2ac65f8065c1aac4cb1489203d5413a106c2777f558076583f8fb43b06fb9fea1a919403c806fc6187b4cd7f0'
